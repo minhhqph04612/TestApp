69dd1d9138c2d90ac08acaaecdd25fcf7dd4dbf6
DESKTOP-36IF0GL\HP <hoangquiminh@gmail.com>
2018-05-01 10:12:00 +0700

    first commit

--- app/src/main/java/com/minh/testapp/MainActivity.java
+++ app/src/main/java/com/minh/testapp/MainActivity.java
@@ -2,12 +2,14 @@ package com.minh.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String name  = " Con ga con";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,name,Toast.LENGTH_SHORT).show();
     }
 }
